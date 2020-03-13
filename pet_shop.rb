dde3569329c3fdfde704a61b451042e035e94487
Ashir <ashirq@outlook.com>
2020-03-13 20:59:48 +0000

    fixed error for remove pet by name

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,49 @@
+@pet_shop = {
+  pets: [
+    {
+      name: "Sir Percy",
+      pet_type: :cat,
+      breed: "British Shorthair",
+      price: 500
+    },
+    {
+      name: "King Bagdemagus",
+      pet_type: :cat,
+      breed: "British Shorthair",
+      price: 500
+    },
+    {
+      name: "Sir Lancelot",
+      pet_type: :dog,
+      breed: "Pomsky",
+      price: 1000,
+    },
+    {
+      name: "Arthur",
+      pet_type: :dog,
+      breed: "Husky",
+      price: 900,
+    },
+    {
+      name: "Tristan",
+      pet_type: :dog,
+      breed: "Basset Hound",
+      price: 800,
+    },
+    {
+      name: "Merlin",
+      pet_type: :cat,
+      breed: "Egyptian Mau",
+      price: 1500,
+    }
+  ],
+  admin: {
+    total_cash: 1000,
+    pets_sold: 0,
+  },
+  name: "Camelot of Pets"
+}
+
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
@@ -41,9 +87,17 @@ def find_pet_by_name(pet_shop, pet_name)
       return pet
     end
   end
-
+  return nil
 end
 
 def remove_pet_by_name(pet_shop, name)
-  pet_shop[:pets][0].delete(name)
+  #pet_list = pet_shop[:pets]
+  #pet_name_match[]
+  pet_shop[:pets].delete_if{|x| x[:name] =- name}
 end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+p remove_pet_by_name(@pet_shop, "Arthur")
