d62eb422f0397545bb9c9d9d92dbcd53b2f4c87d
Ashir <ashirq@outlook.com>
2020-03-13 21:49:46 +0000

    Modified sell pet to customer code to account for when pet not found

--- pet_shop.rb
+++ pet_shop.rb
@@ -78,17 +78,18 @@ end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
 
-  #gather required information
-  name = pet[:name]
-  amount = pet[:price]
+  if pet != nil
+    #gather required information
+    name = pet[:name]
+    amount = pet[:price]
 
-  #assuming only one pet is sold per transaction
-  increase_pets_sold(pet_shop,1)
-
-  remove_customer_cash(customer, amount)
-  add_or_remove_cash(pet_shop, amount)
-  add_pet_to_customer(customer, pet)
-  remove_pet_by_name(pet_shop, name)
+    #assuming only one pet is sold per transaction
+    increase_pets_sold(pet_shop,1)
+    remove_customer_cash(customer, amount)
+    add_or_remove_cash(pet_shop, amount)
+    add_pet_to_customer(customer, pet)
+    remove_pet_by_name(pet_shop, name)
+  end
 
   # assert_equal(1, customer_pet_count(customer))
   # assert_equal(1, pets_sold(@pet_shop))
